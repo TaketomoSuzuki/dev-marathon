cc027e075efc5c81f620f5e97eebc867198bb9de
Suzuki Taketomo <taketomo.suzuki@rakus-partners.co.jp>
2025-10-30 16:25:46 +0900

    EX65_7

--- src/node/index.js
+++ src/node/index.js
@@ -140,7 +140,7 @@ app.get('/cases/:customer_id', async (req, res) => {
   const customer_id = req.params.customer_id;
   try {
     const result = await pool.query(
-      `SELECT case_name, case_status, expected_revenue, representative
+      `SELECT case_id, case_name, case_status, expected_revenue, representative
        FROM cases
        WHERE customer_id = $1
        ORDER BY created_date DESC`,
